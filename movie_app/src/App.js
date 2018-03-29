9605c2abe0ff26c998fc2eefa568fccdd55314b8
seoncheol.kim <seoncheol.kim@navercorp.com>
2018-03-29 23:25:08 +0900

    Loading.. & state change

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -4,54 +4,70 @@ import './App.css';
 import Movie from './Movie';
 
 const movieTitles = [
-	"The Intern",
-	"Old Boy",
-	"Known Girl"
+"The Intern",
+"Old Boy",
+"Known Girl"
 ]
 
 const movieImgs = [
-	"https://goodmenproject.com/wp-content/uploads/2015/09/The-Intern-new-poster.jpg",
-	"https://upload.wikimedia.org/wikipedia/en/6/67/Oldboykoreanposter.jpg",
-	"http://cfile5.uf.tistory.com/image/2578304C56B75E611E9874"
+"https://goodmenproject.com/wp-content/uploads/2015/09/The-Intern-new-poster.jpg",
+"https://upload.wikimedia.org/wikipedia/en/6/67/Oldboykoreanposter.jpg",
+"http://cfile5.uf.tistory.com/image/2578304C56B75E611E9874"
 
-]
-
-const movies = [
-{
-	title: "the Intern",
-	poster: "https://goodmenproject.com/wp-content/uploads/2015/09/The-Intern-new-poster.jpg",
-},
-{
-	title: "Old Boy",
-	poster: "https://upload.wikimedia.org/wikipedia/en/6/67/Oldboykoreanposter.jpg"
-},
-{
-	title: "Known Girl",
-	poster: "http://cfile5.uf.tistory.com/image/2578304C56B75E611E9874"
-}
 ]
 
 
 class App extends Component {
 
+	state = {
+
+	}
+
 	componentWillMount()	{
-		console.log("will mount");
+		setTimeout(()	=>{
+			this.setState({
+				greeting : "Hello!",
+				movies : [
+				{
+					title: "the Intern",
+					poster: "https://goodmenproject.com/wp-content/uploads/2015/09/The-Intern-new-poster.jpg",
+				},
+				{
+					title: "Old Boy",
+					poster: "https://upload.wikimedia.org/wikipedia/en/6/67/Oldboykoreanposter.jpg"
+				},
+				{
+					title: "Known Girl",
+					poster: "http://cfile5.uf.tistory.com/image/2578304C56B75E611E9874"
+				},
+				{
+					title: "Love 911",
+					poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQltX-WDtmG8LUaqqeWvpvcOQSG4OKGjjiBk5TU88fufL3BQnSDgA"
+				}
+				]
+			})
+		}, 5000)
 	}
 
 	componentDidMount()	{
 		console.log("did mount");
 	}
 
-  render() {
-  	console.log("did render");
-    return (
-      <div className="App">
-       	{movies.map( (movie, index) => {
-       		return <Movie title={movie.title} poster={movie.poster} key={index}/>
-       	})}
-      </div>
-    );
-  }
+	_renderMovies = () => {
+		const movies = [this.state.movies.map((movie, index) => {
+			return <Movie title={movie.title} poster={movie.poster} key={index} />
+		})]
+		return movies;
+	}
+
+	render() {
+		console.log("did render");
+		return (
+			<div className="App">
+			{this.state.movies ? this._renderMovies() : "Loading.."}
+			</div>
+			);
+	}
 }
 
-export default App;
+export default App
