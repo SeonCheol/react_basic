f670fcadd196a8ee02c9a484b9f8ba6cee806b02
seoncheol.kim <seoncheol.kim@navercorp.com>
2018-10-09 14:31:59 +0900

    koa server

--- blog/blog-server/src/index.js
+++ blog/blog-server/src/index.js
@@ -1,1 +1,14 @@
-const Koa = 
+const api = require('./api')
+const Koa = require('koa')
+
+const app = new Koa()
+
+app.use(api.routes())
+app.use(ctx => {
+    ctx.body = 'Hello World'
+})
+
+
+app.listen(4000, () => {
+    console.log('listening to port 4000')
+})
