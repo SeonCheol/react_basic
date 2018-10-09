e8b7a2eb682c54ce9d804645689ea043d638acb3
seoncheol.kim <seoncheol.kim@navercorp.com>
2018-10-09 14:16:06 +0900

    code split

--- react-router-tutor/src/App.js
+++ react-router-tutor/src/App.js
@@ -2,16 +2,17 @@ import React, {Component} from 'react';
 import {Route} from 'react-router-dom'
 import {Home, About, Posts} from 'pages'
 import Menu from "./components/Menu";
+import AsyncSplitMe from "./components/AsyncSplitMe";
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <Menu/>
+                <AsyncSplitMe/>
                 <Route exact path='/' component={Home}/>
                 <Route path='/about/:name?' component={About}/>
                 <Route path='/posts' component={Posts}/>
-
             </div>
         )
     }
--- react-router-tutor/src/pages/index.js
+++ react-router-tutor/src/pages/index.js
@@ -1,4 +1,11 @@
-export {default as Home} from './Home'
-export {default as About} from './About'
-export {default as Post} from './Post'
-export {default as Posts} from './Posts'
+// export {default as Home} from './Home'
+// export {default as About} from './About'
+// export {default as Post} from './Post'
+// export {default as Posts} from './Posts'
+
+import asyncComponent from "../lib/asyncComponent";
+
+export const Home = asyncComponent(() => import('./Home'))
+export const About = asyncComponent(() => import('./About'))
+export const Post = asyncComponent(() => import('./Post'))
+export const Posts = asyncComponent(() => import('./Posts'))
